8511a59fe0df735dff02d52450cb9b766dff7373
fizza nayab <tab2baku@gmail.com>
2024-07-15 23:19:29 +0400

    hehe

---
+++ java/java.js
@@ -0,0 +1,24 @@
+
+let FirstName = "Fizza"
+let Surname = "Nayab"
+let bDay = 1999
+let current_year = 2024
+let proqrammingLanguage = "Javascript"
+let Car = "Ferrari"
+let School = "Azerbhaican Dovlet Ressemliq Akademiyasi"
+let Country = "Azerbhiajan"
+let Hobby = "cooking and Gaming"
+let Pet = "Cat(Rio)"
+let Work = "Art teacher"
+let gender = "Female"
+let Movie = "Lord of the Rings"
+let from = "Pakistan"
+let game = "GTA,Minecraft,Resident evil, Last of us, the walking dead.etc"
+
+
+
+
+ console.log(`My name is ${FirstName} ${Surname}. I am ${current_year-bDay} years old. ofcourse I am a ${gender}. I am from ${from}, I am currently living in ${Country} .I  completed my study at ${School}.
+    I am also learning ${ proqrammingLanguage}.My favorite car is ${Car}. My hobbies includes ${Hobby}.My favorite games are ${game}. I also do part time job as ${Work}, if I get some free time I watch movies, one of my favorite ${Movie}. i also have a pet ${Pet}.  `)    
+  
+  
